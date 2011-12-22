5edff6a42363b824c04bf632f5491ab6b7d3e23d
Christian Schneider <cschneider@talend.com>
2011-12-22 15:04:00 +0100

    Formatting and article

--- cxf/personservice/model/src/main/java/net/lr/tutorial/karaf/cxf/personservice/person/PersonService.java
+++ cxf/personservice/model/src/main/java/net/lr/tutorial/karaf/cxf/personservice/person/PersonService.java
@@ -4,13 +4,8 @@ import javax.jws.WebService;
 
 @WebService
 public interface PersonService {
-
     public abstract Person[] getAll();
-
     public abstract Person getPerson(String id);
-
     public abstract void updatePerson(String id, Person person);
-
     public abstract void addPerson(Person person);
-
 }
