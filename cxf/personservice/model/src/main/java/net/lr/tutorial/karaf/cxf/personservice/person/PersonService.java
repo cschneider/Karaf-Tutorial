380fa60bfc1f71a9410eac1d56ac9a0146472800
Christian Schneider <chris@die-schneider.net>
2013-04-26 10:13:16 +0200

    Update to cxf 2.7.4

--- cxf/personservice/model/src/main/java/net/lr/tutorial/karaf/cxf/personservice/person/PersonService.java
+++ cxf/personservice/model/src/main/java/net/lr/tutorial/karaf/cxf/personservice/person/PersonService.java
@@ -4,8 +4,8 @@ import javax.jws.WebService;
 
 @WebService
 public interface PersonService {
-    public abstract Person[] getAll();
-    public abstract Person getPerson(String id);
-    public abstract void updatePerson(String id, Person person);
-    public abstract void addPerson(Person person);
+    public Person[] getAll();
+    public Person getPerson(String id);
+    public void updatePerson(String id, Person person);
+    public void addPerson(Person person);
 }
