96523cbc3a7682d93ef60d3aaf8fad03a1ae03af
Christian Schneider <chris@die-schneider.net>
2014-12-05 09:15:15 +0100

    Remove code to test for a bug

--- tasklist-cdi/persistence/src/main/java/net/lr/tasklist/persistence/impl/InitHelper.java
+++ tasklist-cdi/persistence/src/main/java/net/lr/tasklist/persistence/impl/InitHelper.java
@@ -1,8 +1,5 @@
 package net.lr.tasklist.persistence.impl;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -20,41 +17,9 @@ public class InitHelper {
     
     @PostConstruct
     public void addDemoTasks() {
-        try {
-            taskService.deleteTask(1);
-        }
-        catch (Exception e1) {
-        }
-        Runnable command = new Runnable() {
-            
-            @Override
-            public void run() {
-                taskWatcher();
-            }
-        };
-        ExecutorService newSingleThreadExecutor = Executors.newSingleThreadExecutor();
-        newSingleThreadExecutor.execute(command);
-        try {
-            LOG.info("Adding sample task");
-            taskService.addTask(createTask());
-            LOG.info("Number of tasks is now: " + taskService.getTasks().size());
-        } catch (Exception e) {
-            LOG.warn(e.getMessage(), e);
-        }
+        taskService.addTask(createTask());
     }
     
-    private void taskWatcher() {
-        Thread.currentThread().setName("taskWatcher");
-        for (int c=0; c<10;c++) {
-            LOG.info("Number in check " + taskService.getTasks().size());
-            try {
-                Thread.sleep(1000);
-            }
-            catch (InterruptedException e) {
-            }
-        }
-    }
-
     private Task createTask() {
         Task task1 = new Task();
         task1.setId(1);
--- tasklist-cdi/persistence/src/main/java/net/lr/tasklist/persistence/impl/TaskServiceImpl.java
+++ tasklist-cdi/persistence/src/main/java/net/lr/tasklist/persistence/impl/TaskServiceImpl.java
@@ -13,8 +13,6 @@ import net.lr.tasklist.model.TaskService;
 import org.ops4j.pax.cdi.api.OsgiServiceProvider;
 import org.ops4j.pax.cdi.api.Properties;
 import org.ops4j.pax.cdi.api.Property;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @OsgiServiceProvider(classes = {TaskService.class})
 // The properties below allow to transparently export the service as a web service using Distributed OSGi
@@ -24,7 +22,6 @@ import org.slf4j.LoggerFactory;
 @Singleton
 @Transactional
 public class TaskServiceImpl implements TaskService {
-    Logger LOG = LoggerFactory.getLogger(InitHelper.class);
     
     @PersistenceUnit(unitName="tasklist")
     EntityManager em;
@@ -36,18 +33,8 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public void addTask(Task task) {
-        LOG.info("in transaction " + getTasks().size());
         em.persist(task);
         em.flush();
-        //LOG.info("in transaction " + getTasks().size());
-        try {
-            Thread.sleep(5000);
-        }
-        catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        LOG.info("Now committing");
     }
 
     public Collection<Task> getTasks() {
