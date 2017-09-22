4037a9c22a0b759c4fa0799f9d61c8521e89b575
Christian Schneider <chris@die-schneider.net>
2017-09-22 09:59:08 +0200

    dbng initial

---
+++ dbng/servicejdbc/src/main/java/net/lr/tutorial/db/service/Person.java
@@ -0,0 +1,27 @@
+package net.lr.tutorial.db.service;
+
+public class Person {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d: %s", id, name);
+    }
+}
